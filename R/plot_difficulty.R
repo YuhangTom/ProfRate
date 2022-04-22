4ecfd7e0d80aa53dcd651d939b7411d03869001d
Yuhang (Tom) LIN <yhlin@iastate.edu>
2022-04-21 23:39:41 -0500

    Use ggplot2 to create barplot

--- R/plot_difficulty.R
+++ R/plot_difficulty.R
@@ -2,6 +2,7 @@
 #'
 #' @param url A character value corresponding to the instructor of interest
 #' @import dplyr
+#' @import ggplot2
 #' @export
 #' @examples
 #' plot_difficulty("https://www.ratemyprofessors.com/ShowRatings.jsp?tid=2353616")
@@ -11,13 +12,15 @@ plot_difficulty <- function(url){
   # Get Ratings
   A = ratings_summ(url)
 
-  # Plot
-  factor(A$difficulty, levels = c(1, 2, 3, 4, 5)) %>%
-    table() %>%
-    barplot(col='green',
-            density=15,
-            angle=45,
-            width=5,
-            main = 'Course Difficulty')
+  # Table summary
+  qual <- factor(A$quality, levels = c(1, 2, 3, 4, 5)) %>% table()
+  diff <- factor(A$difficulty, levels = c(1, 2, 3, 4, 5)) %>% table()
+  dat.plot <- tibble(Ratings = rep(1:5, 2) %>% factor(),
+                     Counts = c(qual, diff),
+                     Group = c(rep(1, 5), rep(2, 5)) %>% factor(labels = c("Quality", "Difficulty")))
 
+  # Plot
+  dat.plot %>% ggplot(aes(x = Ratings, y = Counts, fill = Group)) +
+    geom_bar(stat = "identity", position=position_dodge()) +
+    labs(title = "Barplot for course quality and difficulty ratings.")
 }
