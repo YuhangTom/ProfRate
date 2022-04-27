0dbc0f27568f3358a76194ff01b95dd84d0e6b2b
Yuhang (Tom) LIN <yhlin@iastate.edu>
2022-04-27 15:59:43 -0500

    Fix unit test for comment_info()

--- tests/testthat/test-comment_info.R
+++ tests/testthat/test-comment_info.R
@@ -7,16 +7,16 @@ test_that("comment_info inputs work", {
   expect_error(comment_info(c("a", "b")))
 })
 
-# test_that("comment_info outputs work", {
-#   url <- 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid=2036448'
-#   out <- comment_info(url = url)
-#
-#   expect_type(out, "list")
-#   expect_length(out, 2)
-#   expect_named(out, c("word", "n"))
-#   expect_s3_class(out, c("grouped_df", "tbl_df", "tbl", "data.frame"))
-#
-#   expect_type(out$word, "character")
-#
-#   expect_type(out$n, "integer")
-# })
+test_that("comment_info outputs work", {
+  url <- 'https://www.ratemyprofessors.com/ShowRatings.jsp?tid=2036448'
+  out <- comment_info(url = url)
+
+  expect_type(out, "list")
+  expect_length(out, 2)
+  expect_named(out, c("word", "n"))
+  expect_s3_class(out, c("grouped_df", "tbl_df", "tbl", "data.frame"))
+
+  expect_type(out$word, "character")
+
+  expect_type(out$n, "integer")
+})
