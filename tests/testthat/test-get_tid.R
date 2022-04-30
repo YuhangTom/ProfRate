35441e1e159c6093b3573f6f20ae14f34b5d904e
Yuhang (Tom) LIN <yhlin@iastate.edu>
2022-04-30 02:27:31 -0500

    Unit test for get_tid()

---
+++ tests/testthat/test-get_tid.R
@@ -0,0 +1,27 @@
+test_that("get_tid inputs work", {
+  expect_error(get_tid(123))
+  expect_error(get_tid(list(1:2, 1:2)))
+  expect_error(get_tid(data.frame(a = 1:2, b = 3:4)))
+  expect_error(get_tid(matrix(1:4, 2)))
+  expect_error(get_tid(factor(c("a", "b"))))
+  expect_error(get_tid(c("a", "b")))
+})
+
+
+test_that("get_tid outputs work", {
+  name <- "Brakor"
+  out <- get_tid(name = name)
+  expect_type(out, "list")
+  expect_length(out, 4)
+  expect_named(out, c('tID', 'name', 'department', 'university'))
+
+  expect_s3_class(out, c('tbl_df', 'tbl', 'data.frame'))
+
+  expect_type(out$tID, "double")
+
+  expect_type(out$name, "character")
+
+  expect_type(out$department, "character")
+
+  expect_type(out$university, "character")
+})
